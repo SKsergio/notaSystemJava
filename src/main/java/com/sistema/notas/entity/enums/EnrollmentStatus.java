bb805cc8a63931830186347e4a752cb80a288f09
Sksergio <sergiofloress911@gmail.com>
2026-05-03 19:37:32 -0600

    se me quedo este enum

---
+++ src/main/java/com/sistema/notas/entity/enums/EnrollmentStatus.java
@@ -0,0 +1,8 @@
+package com.sistema.notas.entity.enums;
+
+public enum EnrollmentStatus {
+    ACTIVE,      // Cursando actualmente
+    COMPLETED,   // Finalizado
+    WITHDRAWN,   // Se retiró a mitad de año/curso
+    SUSPENDED    // Suspendido por motivos disciplinarios o administrativos
+}
